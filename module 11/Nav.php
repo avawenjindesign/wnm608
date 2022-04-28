6eaf65aa31b1b99840e69ae95c20312bf335690f
avawenjindesign <99636709+avawenjindesign@users.noreply.github.com>
2022-04-28 01:16:05 -0700

    Add files via upload

---
+++ module 11/Nav.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="./css/index.css">
+<header class="header w">
+    <div class="logo">
+        <img src="images/logo.jpg" alt="logo">
+    </div>
+    <div class="nav">
+        <div class="admin-link">
+            <a href="">Login</a>
+            <br>
+            <a href="admin.php">Admin Login</a>
+            <br>
+            <a href="carts.php">Carts</a>
+        </div>
+        <ul>
+            <li>
+                <a href="story.php">BRAND STORY</a>
+            </li>
+            <li>
+                <a href="designers.php">DESIGNERS</a>
+            </li>
+            <li>
+                <a href="product.php">PRODUCT</a>
+            </li>
+            <li>
+                <a href="location.php">LOCATION</a>
+            </li>
+            <li>
+                <a href="contact.php">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</header>
